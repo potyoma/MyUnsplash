216ebbe9ae162ea8fc9be998d9e91ca5ba3756da
Artyom Novoselov <novoselov_artyom@tutanota.com>
2021-01-08 15:04:40 +0300

    changed react layout

---
+++ ClientApp/src/components/Image.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Image extends Component {
+    static image = props.link;
+    
+    render() {
+        return (
+            <div className="container">
+                <img src={this.image} />
+            </div>
+        )
+    }
+}
